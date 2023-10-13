76fc367ab71c62299b455492350ab6caf856f524
Nikki-x <78277236+Nikki-x@users.noreply.github.com>
2023-10-12 21:17:17 -0400

    Add files via upload

---
+++ TextUI.java
@@ -0,0 +1,16 @@
+package connectfour;
+
+public class TextUI{
+
+    private String color = "o";
+
+    public String getColor(){
+        return color;
+
+    }
+
+    // public void setColor(String color){
+    //     this.color = color;
+    // }
+
+}
